dabf9930d53bdc88348e5d462368bbc3c2799b06
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-09-05 16:44:07 +0900

    debug main

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from data_handling.aura_signal_handler import AuraSignalHandler
 from data_handling.csv_aura_data_writer import AuraDataWriter
 
 if __name__ == '__main__':
-    signal_handler = AuraSignalHandler('1', 'EGG')
-    writer = AuraDataWriter('1')
-    for i in range(100):
+    signal_handler = AuraSignalHandler('FISHING')
+    writer = AuraDataWriter('3')
+    for i in range(10):
+        print('writing')
         data = signal_handler.get_data_from_streams()
-        writer.write_data(data[0][0], data[0][1], data[1][0], data[1][1], '')
-        signal_handler.close_streams()
+        aura_data, aura_data_time = data[0]
+        aura_data_eeg, aura_data_eeg_time = data[1]
+        writer.write_data(aura_data, aura_data_time, aura_data_eeg, aura_data_eeg_time, [], 'FISHING')
+
+    signal_handler.close_streams()
+    writer.close_writer()
